75b58b58273d2c68e1cd534ce19cc83d62d3b7fd
Aidan Hamilton <132329147+hammy412@users.noreply.github.com>
2024-06-25 14:46:26 -0400

    cleaned up display functions

--- calculator.js
+++ calculator.js
@@ -14,9 +14,9 @@ function divide(a, b){
     return a / b;
 }
 
-let firstNum;
-let operator;
-let secondNum;
+let firstNum = "";
+let operator = "";
+let secondNum = "";
 
 function operate(a, op, b){
     if (op === '+'){
@@ -34,91 +34,94 @@ const display = document.getElementById("display");
 
 let displayValue; 
 
+function updateDisplay(val){
+    let div = document.createElement("div");
+    div.textContent = val;
+    display.appendChild(div);
+    displayValue += val;
+}
+
 const Btn7 = document.getElementById("7");
 Btn7.addEventListener("click", () => {
-    let div = document.createElement("div");
-    div.textContent = "7";
-    display.appendChild(div);
-    displayValue += "7";
+    updateDisplay("7");
 });
 
 const Btn8 = document.getElementById("8");
 Btn8.addEventListener("click", () => {
-    let div = document.createElement("div");
-    div.textContent = "8";
-    display.appendChild(div);
-    displayValue += "8";
+    updateDisplay("8");
 });
 
 const Btn9 = document.getElementById("9");
 Btn9.addEventListener("click", () => {
-    let div = document.createElement("div");
-    div.textContent = "9";
-    display.appendChild(div);
-    displayValue += "9";
+    updateDisplay("9");
 });
 
 const Btn4 = document.getElementById("4");
 Btn4.addEventListener("click", () => {
-    let div = document.createElement("div");
-    div.textContent = "4";
-    display.appendChild(div);
-    displayValue += "4";
+    updateDisplay("4");
 });
 
 const Btn5 = document.getElementById("5");
 Btn5.addEventListener("click", () => {
-    let div = document.createElement("div");
-    div.textContent = "5";
-    display.appendChild(div);
-    displayValue += "5";
+    updateDisplay("5");
 });
 
 const Btn6 = document.getElementById("6");
 Btn6.addEventListener("click", () => {
-    let div = document.createElement("div");
-    div.textContent = "6";
-    display.appendChild(div);
-    displayValue += "6";
+    updateDisplay("6");
 });
 
 const Btn1 = document.getElementById("1");
 Btn1.addEventListener("click", () => {
-    let div = document.createElement("div");
-    div.textContent = "1";
-    display.appendChild(div);
-    displayValue += "1";
+    updateDisplay("1");
 });
 
 const Btn2 = document.getElementById("2");
 Btn2.addEventListener("click", () => {
-    let div = document.createElement("div");
-    div.textContent = "2";
-    display.appendChild(div);
-    displayValue += "2";
+    updateDisplay("2");
 });
 
 const Btn3 = document.getElementById("3");
 Btn3.addEventListener("click", () => {
-    let div = document.createElement("div");
-    div.textContent = "3";
-    display.appendChild(div);
-    displayValue += "3";
+    updateDisplay("3");
 });
 
 const Btn0 = document.getElementById("0");
 Btn0.addEventListener("click", () => {
-    let div = document.createElement("div");
-    div.textContent = "0";
-    display.appendChild(div);
-    displayValue += "0";
+    updateDisplay("0");
 });
 
-const AC = document.getElementById("AC");
-AC.addEventListener("click", () => {
+const BtnDec = document.getElementById(".");
+BtnDec.addEventListener("click", () => {
+    updateDisplay(".");
+});
+
+function clearDisplay(){
     while (display.hasChildNodes){
         display.removeChild(display.lastChild);
     }
     displayValue = "";
+}
+
+const AC = document.getElementById("AC");
+AC.addEventListener("click", clearDisplay);
+
+const BtnAdd = document.getElementById("+");
+BtnAdd.addEventListener("click", () => {
+    if (firstNum === ""){
+        firstNum = parseFloat(displayValue);
+        operator = "+";
+    } else {
+        operate(firstNum, operator, displayValue)
+    }
+    
 });
+
+const BtnEq = document.getElementById("=");
+BtnEq.addEventListener("click", () => {
+    secondNum = displayValue;
+    
+
+});
+
 
